f8c21baea65eefa336e3914f4d1508f4da3ed8ae
Romina Guimaraens <5071@holbertonschool.com>
2022-07-07 14:52:20 -0700

    betty fix

--- main.h
+++ main.h
@@ -10,9 +10,14 @@ int print_char(va_list list);
 int print_string(va_list list);
 int _putchar(char c);
 
+/**
+ * struct prf - posible character
+ * @letter: letter to check
+ * @f: pointer to list
+ */
 typedef struct prf
 {
 	char letter;
 	int (*f)(va_list);
-}print;
+} print;
 #endif
